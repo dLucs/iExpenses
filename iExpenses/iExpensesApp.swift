dc0efd31cc5aac11bfb1e0d0c1e0248f7f11dbbc
dLucs <lucas_61@hotmail.es>
2024-01-26 11:05:06 +0100

    Initial Commit

---
+++ iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Lucas on 26/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
