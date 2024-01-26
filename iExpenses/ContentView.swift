6ed3706dc27a43f7e5e663555f5d94d32a86b6a9
dLucs <lucas_61@hotmail.es>
2024-01-26 11:25:01 +0100

    main struct and class added, adding and deleting items added

--- iExpenses/ContentView.swift
+++ iExpenses/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+// Create the expense item
+struct expenseItem{
+    let name: String
+    let type: String
+    let amount: Double
+}
+//Create expenses class
+@Observable
+class Expenses {
+    var items = [expenseItem]()
+}
+
 struct ContentView: View {
+    
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            //Iterate over all the items in expenses
+            List{
+                ForEach(expenses.items, id: \.name) {
+                    item in Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpenses")
+            .toolbar{
+                //Add new expense item to expenses
+                Button("Add Expense", systemImage: "plus"){
+                    let expense  = expenseItem(name: "Test", type: "personal", amount: 6)
+                    expenses.items.append(expense)
+                }
+            }
         }
-        .padding()
+        
+    }
+    //remove items at specific offsets
+    func removeItems(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
